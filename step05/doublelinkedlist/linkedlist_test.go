90b268e3afaf117e685d88c6a4ed8334e640abe1
falsystack <falsystack@gmail.com>
2023-05-06 00:47:02 +0900

    test(double-linked-list): implements InsertAfter & InsertBefore

--- step05/doublelinkedlist/linkedlist_test.go
+++ step05/doublelinkedlist/linkedlist_test.go
@@ -50,9 +50,36 @@ func TestPushFront(t *testing.T) {
 	assert.Equal(t, 2, l.Front().Value)
 	assert.Equal(t, 1, l.Back().Value)
 
-	//l.PushFront(3)
-	//assert.Equal(t, 3, l.Front().Value)
-	//assert.Equal(t, 1, l.Back().Value)
-	//
-	//assert.Equal(t, 3, l.O1Count())
+}
+
+func TestInsertAfter(t *testing.T) {
+	var l LinkedList[int]
+
+	l.PushBack(1)
+	l.PushBack(2)
+	l.PushBack(3)
+
+	node := l.GetAt(1)
+	l.InsertAfter(node, 4)
+
+	assert.Equal(t, 4, l.Count())
+	assert.Equal(t, 4, l.GetAt(2).Value)
+	assert.Equal(t, 3, l.Back().Value)
+	assert.Equal(t, 4, node.next.Value)
+	assert.Equal(t, 3, node.next.next.Value)
+}
+
+func TestInsertBefore(t *testing.T) {
+	var l LinkedList[int]
+
+	l.PushBack(1)
+	l.PushBack(2)
+	l.PushBack(3)
+
+	node := l.GetAt(1)
+	l.InsertBefore(node, 4)
+
+	assert.Equal(t, 4, l.Count())
+	assert.Equal(t, 4, l.GetAt(1).Value)
+	assert.Equal(t, 3, l.Back().Value)
 }
