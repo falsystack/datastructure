12a1e35869c8180ac96561065d006d3e95c07c20
falsystack <falsystack@gmail.com>
2023-05-06 23:53:35 +0900

    test(stack): test & benchmark

--- step06/stack/stack_test.go
+++ step06/stack/stack_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestPush(t *testing.T) {
+func TestStack_Push(t *testing.T) {
 	s := New[int]()
 	s.Push(1)
 	s.Push(2)
@@ -14,4 +14,36 @@ func TestPush(t *testing.T) {
 	assert.Equal(t, 3, s.Pop())
 	assert.Equal(t, 2, s.Pop())
 	assert.Equal(t, 1, s.Pop())
+}
+func TestSliceStack_Push(t *testing.T) {
+	s := NewSliceStack[int]()
+	s.Push(1)
+	s.Push(2)
+	s.Push(3)
+
+	assert.Equal(t, 3, s.Pop())
+	assert.Equal(t, 2, s.Pop())
+	assert.Equal(t, 1, s.Pop())
+}
+
+// Loses, 31.26 ns/op
+func BenchmarkLinkedListStack(b *testing.B) {
+	s := New[int]()
+	for i := 0; i < b.N; i++ {
+		s.Push(i)
+	}
+	for i := 0; i < b.N; i++ {
+		s.Pop()
+	}
+}
+
+// Win,  9.781 ns/op
+func BenchmarkSliceStack(b *testing.B) {
+	s := NewSliceStack[int]()
+	for i := 0; i < b.N; i++ {
+		s.Push(i)
+	}
+	for i := 0; i < b.N; i++ {
+		s.Pop()
+	}
 }
