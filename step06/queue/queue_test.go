e7ac3d1cd3a869d99b06c7685b4013f90d396dc8
falsystack <falsystack@gmail.com>
2023-05-06 23:53:28 +0900

    test(queue): test & benchmark

--- step06/queue/queue_test.go
+++ step06/queue/queue_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestPush(t *testing.T) {
+func TestQueue_Push(t *testing.T) {
 	q := New[int]()
 	q.Push(1)
 	q.Push(2)
@@ -14,4 +14,37 @@ func TestPush(t *testing.T) {
 	assert.Equal(t, 1, q.Pop())
 	assert.Equal(t, 2, q.Pop())
 	assert.Equal(t, 3, q.Pop())
+}
+
+func TestSliceQueue_Push(t *testing.T) {
+	q := NewSliceQueue[int]()
+	q.Push(1)
+	q.Push(2)
+	q.Push(3)
+
+	assert.Equal(t, 1, q.Pop())
+	assert.Equal(t, 2, q.Pop())
+	assert.Equal(t, 3, q.Pop())
+}
+
+// Loses, 31.06 ns/op
+func BenchmarkLinkedListQueue(b *testing.B) {
+	s := New[int]()
+	for i := 0; i < b.N; i++ {
+		s.Push(i)
+	}
+	for i := 0; i < b.N; i++ {
+		s.Pop()
+	}
+}
+
+// Win,  9.578 ns/op
+func BenchmarkSliceQueue(b *testing.B) {
+	s := NewSliceQueue[int]()
+	for i := 0; i < b.N; i++ {
+		s.Push(i)
+	}
+	for i := 0; i < b.N; i++ {
+		s.Pop()
+	}
 }
