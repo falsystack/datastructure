7926d934d1579ca01b311933f24d91e8f2aebe40
falsystack <falsystack@gmail.com>
2023-05-17 18:17:38 +0900

    test: benchmark test between quicksort and mergesort

---
+++ step08/sort/bench_test.go
@@ -0,0 +1,28 @@
+package sort
+
+import (
+	"math/rand"
+	"testing"
+)
+
+/*
+QuickSortが１.２倍早い
+*/
+
+func BenchmarkQuickSort(b *testing.B) {
+	arr := make([]int, b.N)
+	for i := 0; i < b.N; i++ {
+		arr[i] = rand.Intn(b.N)
+	}
+
+	QuickSort(arr)
+}
+
+func BenchmarkMergeSort(b *testing.B) {
+	arr := make([]int, b.N)
+	for i := 0; i < b.N; i++ {
+		arr[i] = rand.Intn(b.N)
+	}
+
+	MergeSort(arr)
+}
