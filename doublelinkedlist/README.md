10d21722f3f7f0f3aec24ba55650161a1c941888
falsystack <falsystack@gmail.com>
2023-06-08 14:36:58 +0900

    feat: doublelinkedlist - pushback

---
+++ doublelinkedlist/README.md
@@ -0,0 +1,4 @@
+# Double Linked List
+- Edgeが二つある
+- 業務では主にDouble Linked Listを使う
+- single linked listでは前のnodeを探すためforルーフで全体を回しながら探したがdouble linked listを使うとprev edgeがあるのでforルーフを回さなくても済む
---
+++ doublelinkedlist/doublelinkedlist.go
@@ -0,0 +1,31 @@
+package doublelinkedlist
+
+type Node[T any] struct {
+	next  *Node[T]
+	prev  *Node[T]
+	Value T
+}
+
+type LinkedList[T any] struct {
+	root  *Node[T]
+	tail  *Node[T]
+	count int
+}
+
+func (l *LinkedList[T]) PushBack(val T) {
+	//newNode := new(Node[T])
+	//newNode.Value = val
+	newNode := &Node[T]{
+		Value: val,
+	}
+	l.count++
+
+	if l.root == nil {
+		l.root = newNode
+		l.tail = newNode
+		return
+	}
+
+	l.tail.next = newNode
+	l.tail = newNode
+}
